6bea996d83f45ddb71631be28738bcc1eff0cee1
fmael <mael.fath@ynov.com>
2023-11-09 12:21:43 +0100

    TP4

--- TP4 I'm Socketing r u soketin/code/bs_server_II1.py
+++ TP4 I'm Socketing r u soketin/code/bs_server_II1.py
@@ -7,15 +7,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", action="store", help="open on a specific port")
 args = parser.parse_args()
 
-port = args.port
+port = int(args.port)
 
 if (port == None):
     port = 13337
 
-if (int(port) < 0 or int(port) > 65535):
+if (port < 0 or port > 65535):
     raise ValueError("ERROR Le port spécifié n'est pas un port possible (de 0 à 65535).")
 
-if (int(port) > 0 and int(port) < 1024):
+if (port > 0 and port < 1024):
     raise ValueError("ERROR Le port spécifié est un port privilégié. Spécifiez un port au dessus de 1024.")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
